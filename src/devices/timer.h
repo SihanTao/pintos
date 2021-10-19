1e9b60d838744fcc1a6397a7f56bbf2557dcd51c
st2220 <sihan.tao20@imperial.ac.uk>
2021-10-19 02:35:37 -0700

    [Sihan Tao] Update the task0 implementation of Sihan Tao.

--- src/devices/timer.h
+++ src/devices/timer.h
@@ -3,15 +3,31 @@
 
 #include <round.h>
 #include <stdint.h>
+#include <list.h>
 
 /* Number of timer interrupts per second. */
 #define TIMER_FREQ 100
+
+/*
+ * The structure helps to deal with threads that
+ * wakes up after WAKE_UP_TICKS number of ticks.
+ */
+struct alarm_clock_helper {
+    int64_t wake_up_ticks;
+    /* Can use the semaphore to put the thread
+     * to sleep and unblock the thread
+     */
+    struct semaphore *timer_sema;
+    struct list_elem elem; /* Can be used by list_entry to track the address */
+};
+
 
 void timer_init (void);
 void timer_calibrate (void);
 
 int64_t timer_ticks (void);
 int64_t timer_elapsed (int64_t);
+
 
 /* Sleep and yield the CPU to other threads. */
 void timer_sleep (int64_t ticks);
