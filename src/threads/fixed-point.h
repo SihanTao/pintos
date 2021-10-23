cdabdfaeb51c0e895a5b18cebc77fadfd7781758
Tony Liu <tl2020@gpu25.doc.ic.ac.uk>
2021-10-23 17:07:10 +0100

    [Tony Kevin Jonathan Belle] fixed name collision in fixed point h, and fixed macro definitions

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -3,35 +3,35 @@
 
 #include <stdint.h>
 
-typedef fixed_point_t int32_t
+typedef int32_t fixed_point_t;
 
-#define q 14
-#define f (1 << q)
+#define ___f (0x4000)
+// 1 << 14
 
-#define to_fp (n)((n)*f)
+#define to_fp(n) ((n)*___f)
 
 // convert x to integer rounding toward zero
-#define to_intz (x)((x) / f)
+#define to_intz(x) ((x) / ___f)
 
 // convert x to integer rounding toward nearest
-#define to_intn (x)((x) >= 0            \
-                        ? (x) + (f / 2) \
-                        : (x) - (f / 2))
+#define to_intn(x) ((x) >= 0            \
+                        ? (x) + (___f / 2) \
+                        : (x) - (___f / 2))
 
-#define fp_add (x, y)((x) + (y))
+#define fp_add(x, y) ((x) + (y))
 
-#define fp_sub (x, y)((x) - (y))
+#define fp_sub(x, y) ((x) - (y))
 
-#define fp_int_add (x, n)(fp_add(x, to_fp(n)))
+#define fp_int_add(x, n) (fp_add(x, to_fp(n)))
 
-#define fp_int_sub (x, n)(fp_sub(x, to_fp(n)))
+#define fp_int_sub(x, n) (fp_sub(x, to_fp(n)))
 
-#define fp_mul (x, y)(((int64_t)(x)) * to_intz(y))
+#define fp_mul(x, y) (((int64_t)(x)) * to_intz(y))
 
-#define fp_int_mul (x, n)((x) * (n))
+#define fp_int_mul(x, n) ((x) * (n))
 
-#define fp_div (x, y)(((int64_t)(x)) * f / (y))
+#define fp_div(x, y) (((int64_t)(x)) * ___f / (y))
 
-#define fp_int_div (x, n)((x) / (n))
+#define fp_int_div(x, n) ((x) / (n))
 
 #endif // _THREAD_FIXED_POINT_H
