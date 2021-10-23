07ba178a49245c4222cb125da620e02c8026ab19
Tony Liu <tl2020@gpu25.doc.ic.ac.uk>
2021-10-23 15:57:53 +0100

    [Tony Kevin Jonathan Belle] initialized thread niceness

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -90,6 +90,7 @@ struct thread
     int priority;                       /* Priority. */
     struct list list_of_locks;
     struct list_elem allelem;           /* List element for all threads list. */
+    int nice;
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
