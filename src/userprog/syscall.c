55414b2f27f2badc62a8ac36882f7cde73c4190b
Sihan Tao (st2220) <sihan.tao20@imperial.ac.uk>
2021-11-05 11:21:02 -0700

    [Sihan Tao]Implement a function to ensure the safe user memory access.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -18,3 +18,26 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
   thread_exit ();
 }
+
+/*
+ * Check whether the VADDR is safe.
+ * Otherwise exit the thread.
+ */
+void* check_safe_memory_access(const void* vaddr)
+{
+  struct thread * cur = thread_current();
+
+  if (!is_user_vaddr(vaddr)) {
+    thread_exit();
+  }
+
+  void * kaddr = pagedir_get_page(cur->pagedir, vaddr);
+
+  if (kaddr == NULL)
+  {
+    thread_exit();
+  }
+
+  return kaddr;
+  
+}
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,5 +1,8 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
+#define USERPROG
+
+#include "pagedir.h"
 
 void syscall_init (void);
 
